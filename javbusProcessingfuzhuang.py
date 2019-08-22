4a1d56b11cdac0af7bdda4d6bb1359d1629f6d2b
安哲铭 <foxlowm@foxmail.com>
2019-08-22 09:36:49 +0800

    Add files via upload

--- javbusProcessingfuzhuang.py
+++ javbusProcessingfuzhuang.py
@@ -8,94 +8,98 @@
 
 import asyncio, aiofiles
 import os
-import sys
-import re
+import pickle
 from pyquery import PyQuery as pq
 from tools import Web
-from multiprocessing import Pool,Process
 
-cookies = {
-    "__cfduid": "d00908d789c4ace727d36a6249928efa31565789625",
-    "PHPSESSID": "5jd5625obsa8d45jhud5c81762",
-    "existmag": "all",
-}
+with open("AllTags.txt", 'rb') as file:
+    dictTag = pickle.load(file)
 
-async def Producter(url, q):
-    """生产者函数，迭代解析下一页"""
-    w = Web(url, cookies=cookies)
+class javbus(object):
 
-    htmlCode = await w.getHtmlCode()
-    # 解析链接并放入异步队列
-    for a in pq(htmlCode)('a.movie-box'):
-        await q.put(pq(a).attr('href'))
+    def __init__(self):
+        self.dictTag = dictTag
+        self.cookies = {
+                       "__cfduid": "d00908d789c4ace727d36a6249928efa31565789625",
+                       "PHPSESSID": "5jd5625obsa8d45jhud5c81762",
+                       "existmag": "all",
+                          }
+    @classmethod
+    async def Producter(self,url, q):
+        """生产者函数，迭代解析下一页"""
+        w = Web(url, cookies=self.cookies)
 
-    # 下一页判断
-    try:
-        nxtpage = "https://www.busjav.net"+pq(htmlCode)('a#next').attr('href')
-        if nxtpage != url:  # 逻辑不为空且不等于当前请求地址
-            print(nxtpage)
-            await Producter(nxtpage, q)
-    except TypeError:
-        print(url+"\tLast Page")
+        htmlCode = await w.getHtmlCode()
+        # 解析链接并放入异步队列
+        for a in pq(htmlCode)('a.movie-box'):
+            await q.put(pq(a).attr('href'))
 
-async def Producter2(q, q2):
-    while True:
+        # 下一页判断
         try:
-            boxItem = await q.get()
-            #print(boxItem)
-            w2 = Web(boxItem)
+            nxtpage = "https://www.busjav.net"+pq(htmlCode)('a#next').attr('href')
+            if nxtpage != url:  # 逻辑不为空且不等于当前请求地址
+                #print(nxtpage)
+                await self.Producter(nxtpage, q)
+        except TypeError:
+            print(url+"\tLast Page")
 
-            htmlCode = await w2.getHtmlCode()
-            for _ in pq(htmlCode)('a.bigImage'):
-                await q2.put([boxItem,pq(_).attr('href')])
-        except:
-            pass
-        finally:
-            if q.empty():
-                break
+    @classmethod
+    async def Producter2(self, q, q2):
+        while True:
+            try:
+                boxItem = await q.get()
+                w2 = Web(boxItem)
 
+                htmlCode = await w2.getHtmlCode()
+                for _ in pq(htmlCode)('a.bigImage'):
+                    await q2.put([boxItem,pq(_).attr('href')])
+            except:
+                pass
+            finally:
+                if q.empty():
+                    break
+    
+    @classmethod
+    async def Consumer(self, q2, url):
+        """消费者协程，用以下载图片，可多开"""
+        while True:
+            try:
+                imgUrl = await q2.get()
+                w3 = Web(imgUrl[1])
+                tag = url.split('/')[-1]
+                typeMa = url.split('/')[3]
+                dirName = f"javbus/{typeMa}/{self.dictTag[typeMa][tag]}"
+                Web.mkDir(dirName)
 
-async def Consumer(q2, tag):
-    """消费者协程，用以下载图片，可多开"""
-    while True:
-        try:
-            imgUrl = await q2.get()
-            w3 = Web(imgUrl[1])
-            dirName = f"javbus/有码/服装/{tag}"
-            Web.mkDir(dirName)
-
-            data = await w3.getByte()
-            imgName = f"{dirName}/{imgUrl[0].split('/')[-1]}.jpg"
-            if not os.path.exists(imgName):
-                async with aiofiles.open(imgName, 'wb') as f:
-                    await f.write(data)
-            else:
-                continue
-        except:
-            pass
-        finally:
-            if q2.empty():
-                break
+                data = await w3.getByte()
+                imgName = f"{dirName}/{imgUrl[0].split('/')[-1]}.jpg"
+                print(imgName)
                 
+                if not os.path.exists(imgName):
+                    async with aiofiles.open(imgName, 'wb') as f:
+                        await f.write(data)
+                else:
+                    continue
+            except:
+                pass
+            finally:
+                if q2.empty():
+                    break
 
-async def Main(url):
-    q = asyncio.Queue()
-    q2 = asyncio.Queue()
+    async def Main(self, url):
+        q = asyncio.Queue()
+        q2 = asyncio.Queue()
 
-    tag = url.split('/')[-1]
-    task1 = [asyncio.create_task(Producter(url, q))]
-    task2 = [asyncio.create_task(Producter2(q, q2)) for _ in range(256)]
-    task3 = [asyncio.create_task(Consumer(q2, tag)) for tmp in range(256)]
+        task1 = [asyncio.create_task(self.Producter(url, q))]
+        task2 = [asyncio.create_task(self.Producter2(q, q2)) for _ in range(72)]
+        task3 = [asyncio.create_task(self.Consumer(q2, url)) for tmp in range(72)]
 
-    await asyncio.wait(task1 + task3 + task2)
+        await asyncio.wait(task1 + task3 + task2)
 
 
 if __name__ == "__main__":
     #有码服装
     urlList = [
-        "https://www.busjav.net/genre/k",
-        "https://www.busjav.net/genre/i",
-        "https://www.busjav.net/genre/a",
         "https://www.busjav.net/genre/9",
         "https://www.busjav.net/genre/8",
         "https://www.busjav.net/genre/1",
@@ -103,4 +107,5 @@ if __name__ == "__main__":
         "https://www.busjav.net/genre/5x",
     ]
     for _ in urlList:
-        asyncio.run(Main(_))
+        a = javbus()
+        asyncio.run(a.Main(_))
--- javbusProcessingxingwei.py
+++ javbusProcessingxingwei.py
@@ -6,82 +6,8 @@
 # @Last Modified time: 2019-07-02 星期三 22:08
 
 
-import asyncio, aiofiles
-import os
-import sys
-import re
-from pyquery import PyQuery as pq
-from tools import Web
-from multiprocessing import Pool,Process
-
-async def Producter(url, q):
-    """生产者函数，迭代解析下一页"""
-    w = Web(url)
-
-    htmlCode = await w.getHtmlCode()
-    # 解析链接并放入异步队列
-    for a in pq(htmlCode)('a.movie-box'):
-        await q.put(pq(a).attr('href'))
-
-    # 下一页判断
-    try:
-        nxtpage = "https://www.busjav.net"+pq(htmlCode)('a#next').attr('href')
-        if nxtpage is not None and nxtpage != url:  # 逻辑不为空且不等于当前请求地址
-            print(nxtpage)
-            await Producter(nxtpage, q)
-    except TypeError:
-        print("Last Page")
-
-async def Producter2(q, q2):
-    while True:
-        try:
-            boxItem = await q.get()
-            print(boxItem)
-            w2 = Web(boxItem)
-
-            htmlCode = await w2.getHtmlCode()
-            for _ in pq(htmlCode)('a.bigImage'):
-                await q2.put([boxItem,pq(_).attr('href')])
-        except:
-            pass
-        finally:
-            if q.empty():
-                break
-
-
-async def Consumer(q2, tag):
-    """消费者协程，用以下载图片，可多开"""
-    while True:
-        try:
-            imgUrl = await q2.get()
-            w3 = Web(imgUrl[1])
-            dirName = f"javbus/有码/行为/{tag}"
-            Web.mkDir(dirName)
-
-            data = await w3.getByte()
-            imgName = f"{dirName}/{imgUrl[0].split('/')[-1]}.jpg"
-            if not os.path.exists(imgName):
-                async with aiofiles.open(imgName, 'wb') as f:
-                    await f.write(data)
-            else:
-                continue
-        except:
-            pass
-        finally:
-            if q2.empty():
-                break
-                
-
-async def Main(url):
-    q = asyncio.Queue()
-    q2 = asyncio.Queue()
-
-    tag = url.split('/')[-1]
-    task1 = [asyncio.create_task(Producter(url, q))]
-    task2 = [asyncio.create_task(Producter2(q, q2)) for _ in range(256)]
-    task3 = [asyncio.create_task(Consumer(q2, tag)) for tmp in range(256)]
-
-    await asyncio.wait(task1 + task3 + task2)
+import asyncio
+from javbusProcessingfuzhuang import javbus
 
 if __name__ == "__main__":
     #有码行为
@@ -116,5 +42,5 @@ if __name__ == "__main__":
     ]
 
     for _ in urlList:
-        asyncio.run(Main(_))
-
+        a = javbus()
+        asyncio.run(a.Main(_))
