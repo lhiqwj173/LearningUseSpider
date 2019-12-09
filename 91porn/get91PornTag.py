49a4fb0878e105cbeafe8b5138997f739621d32f
John Han <foxlowm@foxmail.com>
2019-12-09 15:59:23 +0800

    Add files via upload

--- 91porn/get91PornTag.py
+++ 91porn/get91PornTag.py
@@ -3,25 +3,20 @@
 # @Email: foxlowm@gmail.com
 # @Date:   2019-12-09 15:09:39
 # @Last Modified by:   anzeme
-# @Last Modified time: 2019-12-09 15:44:09
+# @Last Modified time: 2019-12-09 15:59:01
 
 
 from tools import *
 import pickle
 
-# d = {}
-# async def main():
-#     r = await get_html_code('https://91avv8.com/tags/', proxy='http://127.0.0.1:10801')
-#     # print(r)
-#     for _ in pq(r)('#main.large-9.medium-9.columns')('a'):
-#         d.setdefault(pq(_).text(), pq(_).attr('href'))
+d = {}
+async def main():
+    r = await get_html_code('https://91avv8.com/tags/', proxy='http://127.0.0.1:10801')
+    # print(r)
+    for _ in pq(r)('#main.large-9.medium-9.columns')('a'):
+        d.setdefault(pq(_).text(), pq(_).attr('href'))
 
-#     with open('91pornTags', 'wb') as file:
-#         pickle.dump(d, file)
-# asyncio.run(main())
-
-with open('91pornTags', 'rb') as file:
-    d = pickle.load(file)
-
-print(d)
+    with open('91pornTags', 'wb') as file:
+        pickle.dump(d, file)
+asyncio.run(main())
 
