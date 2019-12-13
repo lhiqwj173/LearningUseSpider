8d974a3e40d1b931f082e2a5e334cc63052088a6
John Han <foxlowm@foxmail.com>
2019-12-13 12:08:29 +0800

    Add files via upload

--- spiderModel.py
+++ spiderModel.py
@@ -3,83 +3,70 @@
 # @Software: vscode
 # @Date: 2019-06-25 星期三 22:26
 # @Last Modified by:   anzeme
-# @Last Modified time: 2019-12-09 16:06:15
-
-import asyncio, sys
-from pyquery import PyQuery as pq
+# @Last Modified time: 2019-12-13 12:07:56
 from tools import *
-import re
-from functools import partial
+if os.name == "posix":
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-# parse = argparse.ArgumentParser(
-#     description="xxxxx", prog='xxxxx.py')
-# # parse.add_argument('-t', '--tag', type=str, metavar='tagname', help="the tag you wish, example: -t cosplay")
-# parse.add_argument('-p', '--page', metavar='n', type=str, help="which page to crawl(clash with -b -a), example: -p 12")
-# parse.add_argument('-b', '--between', type=str, nargs=2, metavar='n',
-#                    help="page between a and b(include, clash with -p -a), example: -b 2 3")
-# parse.add_argument('-a', '--all', action='store_true', help="this flag would download all the page(clash with -p -b)")
-# args = parse.parse_args()
+parse = argparse.ArgumentParser(
+    description="downloader for fc2ppvfanclub picture, default runtime crawl the first page", prog='xxxxxxxx.py')
+parse.add_argument('-p', '--page',default='1', metavar='指定页数', type=str, help="指定页数进行下载")
+parse.add_argument('-e', '--end', type=str, metavar='结束页数', help="指定结束页数")
+parse.add_argument('-a', '--all', action='store_true', help="下载全站")
+parse.add_argument('--proxy', type=str, metavar='"地址:端口"', help="启用代理，暂不支持socks,example: --proxy=\"http://127.0.0.1:1080\"")
+args = parse.parse_args()
 
-# if args.between is not None:
-#     a, b = args.between
-#     if b < a:
-#         print("页数b必须大于a")
-#         sys.exit(0)
-# else:
-#     a, b = None, None
-# if args.all is True and args.between is not None:
-#     print("a,b标签只能选一个")
-#     sys.exit(0)
-#     # raise RuntimeError("a,b标签只能选一个")
-# if args.between is not None and args.page is not None:
-#     print("p,b标签只能选一个")
-#     sys.exit(0)
+    # raise RuntimeError("p,b标签只能选一个")
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+}
 
-# if args.page is None:
-#     args.page = '1'
-#     # raise RuntimeError("p,b标签只能选一个")
 
-# 主页
-hostname = 'xxxxxxxxxxxxxxxxxxxxxx'
-proxy='http://xxx.xxx.xxx.xxx:xxx'
+hostname = "xxxxxxxxxxxxxx" # 自行修改
 
-get_html_code_proxy = partial(get_html_code, proxy=proxy)
-
+if args.proxy != None:
+    get_html_code_proxy = partial(get_html_code, proxy=args.proxy, headers=headers)
+    get_byte_named_proxy = partial(get_byte_named, proxy=args.proxy, headers=headers)
+else:
+    get_html_code_proxy = partial(get_html_code, headers=headers)
+    get_byte_named_proxy = partial(get_byte_named, headers=headers)
 
 async def crawler(url, q):
     """生产者函数，迭代解析下一页"""
     try:
         r = await get_html_code_proxy(url)
 
-        # 解析链接并放入异步队列
-
+        # 解析链接并放入异步队列，自行修改解析目标
         for a in pq(r)(".video-list.row.small-up-1.medium-up-2.large-up-4")('.column'):
             if pq(a)('a').attr('href') is not None:
                 await q.put(pq(a)('a').attr('href'))
 
-        #下一页判断
-        nxtpage = pq(r)('.pagination.text-center.margin-top-4')('li').eq(-2)('a').attr('href')
-        if nxtpage not in all_ready_url and nxtpage is not None:  # 逻辑不为空且不等于当前请求地址
-           print(nxtpage)
-           await crawler(nxtpage, q)
-    except:
-        pass
-    finally:
-        pass
+        if args.all is True:
+            #下一页判断，自行修改
+            nxtpage = pq(r)('.pagination.text-center.margin-top-4')('li').eq(-2)('a').attr('href')
+            if nxtpage is not None and nxtpage != url:  # 逻辑不为空且不等于当前请求地址
+               print(nxtpage)
+               await crawler(nxtpage, q)
+
+    except BaseException as e:
+        print(e)
 
 async def downloader(q):
     """消费者协程，用以下载图片，可多开"""
     while True:
         try:
-            l = await q.get()
-            r = await get_html_code_proxy(l)
+            # 如果不需要二次解析的话
+            filesavepath="xxxxxxxxxxxxx" # 自行修改
+            await get_byte_named_proxy(session, await q.get(), filesavepath)
 
-            video_url = re.findall("(http://vk.lowbb.xyz/medias/\S+)\"", r)[0]
-            with open('file.txt', 'a+') as file:
-                file.write(f"{video_url}\n")
+            #r = await get_html_code_proxy(await q.get())
+            #url = xxxxxxxxxxxxxxxxxxxxx
+            #filesavepath="xxxxxxxxxxxxx"
+            #await get_byte_named_proxy(session, url, filesavepath)
 
-        except:
-            pass
+        except BaseException as e:
+            print(e)
         finally:
             if q.empty():
                 break
@@ -89,9 +76,9 @@ async def Main():
 
     task1 = asyncio.create_task(crawler(hostname, q))
     #下载协程数量一定要比一次爬取得到的资源数小，否则无法正常结束
-    task2 = [asyncio.create_task(downloader(q)) for tmp in range(32)]
-
-    await asyncio.wait([task1] + task2)
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        task2 = [asyncio.create_task(downloader(q, session)) for tmp in range(8)]
+        await asyncio.wait([task1] + task2)
 
 if __name__ == '__main__':
     asyncio.run(Main())
--- tools.py
+++ tools.py
@@ -1,8 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: anzeme
+# @Email: foxlowm@gmail.com
+# @Date:   2019-10-08 01:19:20
+# @Last Modified by:   anzeme
+# @Last Modified time: 2019-12-13 12:06:10
+
 import aiohttp
 import os
 import asyncio
 import aiofiles
-import re, sys, argparse
+import re, sys, argparse, pprint, pickle
 from pyquery import PyQuery as pq
 from functools import partial
 
@@ -12,38 +19,34 @@ def mk_dir(dir):
         os.makedirs(dir)
 
 
-async def _fetch(url, **kwargs):
+async def get_html_code(url, **kwargs):
     """解析函数"""
     try:
         async with aiohttp.ClientSession(trust_env=True) as session:
             async with session.get(url, proxy=kwargs.get('proxy'), headers=kwargs.get('headers'), cookies=kwargs.get('cookies'), verify_ssl=False) as response:
-                if kwargs.get('data') == True:
-                    return await response.read()
-                else:
                     return await response.text()
     except BaseException as e:
         print(e)
 
-async def get_html_code(url, **kwargs):
-    return await _fetch(url, **kwargs)
+# async def get_html_code(url, **kwargs):
+#     """请求通常是单协程跑，所以不需要单独开一个session"""
+#     return await _fetch(url, **kwargs)
 
-async def get_byte(url, **kwargs):
-    return await _fetch(url, data=True, **kwargs)
-
-async def get_byte_named(session, url, filepath, **kwargs):
+async def get_byte_named(session, url, fileSavePath, **kwargs):
+    """下载函数，如果关闭太快的会session会报错，单独在外层开个session调用"""
     try:
         r = await session.get(url, proxy=kwargs.get('proxy'), headers=kwargs.get('headers'), verify_ssl=False, timeout=60)
-        if os.path.exists(filepath):
-            if str(os.path.getsize(filepath)) == str(r.headers.get('Content-Length')):
+        if os.path.exists(fileSavePath):
+            if str(os.path.getsize(fileSavePath)) == str(r.headers.get('Content-Length')):
                 #await session.close()
-                raise BaseException(f"文件已存在: {filepath}")
+                raise BaseException(f"文件已存在: {fileSavePath}")
         async with aiofiles.open("result.txt", 'a+') as file:
-            await file.write(f"{url}\t{filepath}\n")
-        async with aiofiles.open(filepath, 'wb') as file:
+            await file.write(f"{url}\t{fileSavePath}\n")
+        async with aiofiles.open(fileSavePath, 'wb') as file:
             while True:
                 chunk = await r.content.read(2048)
                 if not chunk:
-                    print(filepath+" Done")
+                    print(fileSavePath+" Done")
                     break
                 await file.write(chunk)
     except asyncio.TimeoutError as e:
